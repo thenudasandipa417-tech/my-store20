aa3dad021144cea38233549e1960389fd1f0250f
thenudasandipa417-tech <thenudasandipa417@gmail.com>
2026-07-31 11:40:45 +0530

    Delete my-store directory

--- my-store/products.js
+++
@@ -1,6 +0,0 @@
-const products=[
-  {name:'Blue T-Shirt',price:650,category:'Clothing',image:'blue.jpeg'},
-  {name:'Brown T-Shirt',price:650,category:'Clothing',image:'brown.jpeg'},
-  {name:'Dark Brown T-Shirt',price:650,category:'Clothing',image:'darkbrown.jpeg'},
-  {name:'Light Blue T-Shirt',price:650,category:'Clothing',image:'lightblue.jpeg'},
-               ];
