644fa6ac2ab03d2bd8d12c56d2dad14df9a2fa2f
thenudasandipa417-tech <thenudasandipa417@gmail.com>
2026-07-31 11:23:35 +0530

    Update products.js

--- my-store/products.js
+++ my-store/products.js
@@ -1,1 +1,6 @@
-const products=[{name:'T-Shirt',price:10,category:'Clothing',image:'images/tshirt.jpg'}];
+const products=[
+  {name:'Blue T-Shirt',price:650,category:'Clothing',image:'blue.jpeg'},
+  {name:'Brown T-Shirt',price:650,category:'Clothing',image:'brown.jpeg'},
+  {name:'Dark Brown T-Shirt',price:650,category:'Clothing',image:'darkbrown.jpeg'},
+  {name:'Light Blue T-Shirt',price:650,category:'Clothing',image:'lightblue.jpeg'},
+               ];
